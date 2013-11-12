fdd078fcc92c8e935b37bb6c4341cbf102f3f69b
Tomas Babej <tomasbabej@gmail.com>
2013-11-12 17:19:52 +0100

    actor: Extend actor to load all the plugins

--- actor.py
+++ actor.py
@@ -5,22 +5,23 @@ import logging
 import time
 
 import dbus
-import gobject
 import dbus.mainloop.glib
 
 
 from yapsy.PluginManager import PluginManager
 
-from plugins import IReporter
+from plugins import IReporter, IChecker, IFixer
 
 
 class Actor(object):
 
     def __init__(self):
         self.plugin_manager = PluginManager()
-        self.plugin_manager.setPluginPlaces(["reporters"])
+        self.plugin_manager.setPluginPlaces(["reporters", "checkers", "fixers"])
         self.plugin_manager.setCategoriesFilter({
            "Reporter": IReporter,
+           "Checker": IChecker,
+           "Fixer": IFixer,
            })
         self.plugin_manager.collectPlugins()
 
@@ -32,6 +33,14 @@ class Actor(object):
     def check_reporters(self):
         for reporter in self.get_plugins("Reporter"):
             print reporter.report()
+
+    def check_checkers(self):
+        for checker in self.get_plugins("Checker"):
+            print checker.check()
+
+    def check_fixers(self):
+        for fixer in self.get_plugins("Fixer"):
+            print fixer.fix()
 
     def do_main_program(self):
         self.check_reporters()
@@ -45,8 +54,12 @@ if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     actor = Actor()
-    print actor.plugin_manager.getAllPlugins()
+    print actor.get_plugins("Reporter")
+    print actor.get_plugins("Checker")
+    print actor.get_plugins("Fixer")
 
     while True:
         time.sleep(5)
         actor.check_reporters()
+        actor.check_checkers()
+        actor.check_fixers()
