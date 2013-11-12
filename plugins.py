cf98838f8583dcb9b413cb6dab0da9adf286fc74
Tomas Babej <tomasbabej@gmail.com>
2013-11-12 17:16:43 +0100

    plugins: Create IChecker and IFixer plugin base classes

--- plugins.py
+++ plugins.py
@@ -3,8 +3,40 @@ from yapsy.IPlugin import IPlugin
 
 class IReporter(IPlugin):
     """Reports user activity to the AcTor"""
+    def __init__(self, **options):
+        self.options = options
 
     def report(self):
-        """Returns user activity key"""
+        """Returns user activity value"""
         pass
 
+
+class IChecker(IPlugin):
+    """Evaluates user activity depending on the input from the responders"""
+
+    def __init__(self, **options):
+        self.options = options
+
+    def check(self, **reports):
+        """
+        Returns evaluation of the situation - true or false, accorgding
+        to the input from the reporters, passed to the check function,
+        and custom logic of Checker itself.
+
+        This function should return a dictionary, with mandatory key result
+        which holds the value of the check itself, and any optional keys that
+        are then passed to the fixer.
+        """
+        pass
+
+
+class IFixer(IPlugin):
+    def __init__(self, **options):
+        self.options = options
+
+    def fix(self, **data):
+        """
+        This function does execute the given action.
+        """
+
+        pass
