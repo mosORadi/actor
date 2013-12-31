87c020bd5a54bfb8ae8928d5f806e676721fbc20
Tomas Babej <tomasbabej@gmail.com>
2013-12-31 12:28:59 +0100

    checkers: Add Hamster Activity Daily Duration checker

---
+++ checkers/hamster_activity_duration.py
@@ -0,0 +1,33 @@
+from plugins import IChecker
+
+import re
+
+class HamsterActivityDailyDurationChecker(IChecker):
+    """
+    Checks whether a time limit for a particular activity has not been
+    exceeded.
+
+    Expects reports:
+      - hamster_activity_daily_duration - Input dictionary of summary of
+                                          activity duration
+
+    Options:
+      - limit : The activity duration limit, in minutes
+    """
+
+    export_as = 'hamster_activity_duration'
+
+    def __init__(self, **options):
+        super(HamsterActivityDailyDurationChecker, self).__init__(**options)
+        self.activity = self.options.get('activity')
+        self.limit = self.options.get('limit', 0) * 60
+
+        if not self.activity:
+            raise ValueError("You must specify the activity option for HamsterActivityDailyDurationChecker")
+        if not self.limit:
+            raise ValueError("You must specify the limit option for HamsterActivityDailyDurationChecker")
+
+    def check(self, **reports):
+        duration = reports['hamster_activity_daily_duration'].get(self.activity, 0)
+        
+        return duration > self.limit
