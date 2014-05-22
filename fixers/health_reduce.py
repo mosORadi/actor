584192a32c43cc64e524c670890dcd00f7aa1859
Tomas Babej <tomasbabej@gmail.com>
2014-05-23 00:56:07 +0200

    fixers: Add HealthDecreaseFixer

---
+++ fixers/health_reduce.py
@@ -0,0 +1,33 @@
+from plugins import IFixer
+import dbus
+import dbus.service
+
+import random
+
+class HealthDecreaseSignal(dbus.service.Object):
+
+    def __init__(self):
+        dbus.service.Object.__init__(self, dbus.SessionBus(), '/org/freedesktop/Actor/%s' % random.randint(1,100000))
+
+    @dbus.service.signal(dbus_interface='org.freedesktop.AcTor', signature='s')
+    def HealthDecreaseSignal(self, countdown_id):
+        return countdown_id
+
+class HealthDecreaseFixer(IFixer):
+    """
+    Emits a D-Bus signal to decrease health on HealthChecker with
+    the specified identifier.
+
+    Required options:
+        id - HealthChecker identifier
+    """
+
+    export_as = 'health_decrease'
+    required_plugin_options = ['id']
+
+    def __init__(self, **options):
+        super(HealthDecreaseFixer, self).__init__(**options)
+        self.signal = HealthDecreaseSignal()
+
+    def fix(self, **reports):
+        self.signal.HealthDecreaseSignal(self.options.get('id', ''))
