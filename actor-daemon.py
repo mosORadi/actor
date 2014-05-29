9b48ce4a54ef13c0ffbc35db59cb094775a1e23f
Tomas Babej <tomasbabej@gmail.com>
2014-05-29 21:31:40 +0200

    actor-daemon: Get DBUS session bus address from environment of configurable desktop process

--- actor-daemon.py
+++ actor-daemon.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+import local_config
 import config
 import os
+import subprocess
 import sys
 
 from daemon import runner
@@ -14,6 +16,17 @@ class ActorDaemon(object):
         self.stderr_path = '/dev/tty'
         self.pidfile_path = os.path.join(config.HOME_DIR, 'actor.pid')
         self.pidfile_timeout = 5
+
+    def set_environ(self):
+        desktop_pid = subprocess.check_output('/usr/sbin/pidof %s' %
+                                              local_config.DESKTOP_PROCESS,
+                                              shell=True).strip()
+        with open('/proc/%s/environ' % desktop_pid) as f:
+             desktop_environ = dict(map(lambda x: x.split('=', 1),
+                                   [var for var in f.read().split('\0')
+                                    if '=' in var]))
+             os.putenv('DBUS_SESSION_BUS_ADDRESS',
+                       desktop_environ['DBUS_SESSION_BUS_ADDRESS'])
 
     def run(self):
         # Since Actor can connect from plugins to sockets,
@@ -28,6 +41,9 @@ app = ActorDaemon()
 if not sys.stdout.isatty():
     app.stdout_path = '/dev/null'
     app.stderr_path = '/dev/null'
+
+if os.environ.get('DBUS_SESSION_BUS_ADDRESS', None):
+    app.set_environ()
 
 daemon_runner = runner.DaemonRunner(app)
 daemon_runner.do_action()
