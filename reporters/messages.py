92acad521c1b4ef84578d8a55c17552e476227d0
Tomas Babej <tomasbabej@gmail.com>
2014-09-24 18:27:30 +0200

    reporters: Properly fail if Pidgin is not available in MessageReporter

--- reporters/messages.py
+++ reporters/messages.py
@@ -13,14 +13,21 @@ class MessagesReporter(IReporter):
     def __init__(self, **options):
         super(MessagesReporter, self).__init__(**options)
 
-        self.bus = dbus.SessionBus()
-        proxy = self.bus.get_object("im.pidgin.purple.PurpleService",
-                                    "/im/pidgin/purple/PurpleObject")
-        self.pidgin = dbus.Interface(
-                          proxy,
-                          dbus_interface="im.pidgin.purple.PurpleInterface")
+        try:
+            self.bus = dbus.SessionBus()
+            proxy = self.bus.get_object("im.pidgin.purple.PurpleService",
+                                        "/im/pidgin/purple/PurpleObject")
+            self.pidgin = dbus.Interface(
+                proxy,
+                dbus_interface="im.pidgin.purple.PurpleInterface"
+            )
+        except dbus.exceptions.DBusException:
+            self.pidgin = None
 
     def get_message_senders(self):
+        if not self.pidgin:
+            return []
+
         messages = self.pidgin.PurpleGetIms()
         names = [self.pidgin.PurpleConversationGetName(m)
                  for m in messages]
