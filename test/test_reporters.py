01420e89107ccfe507f52e168434e3fa5af4f24a
Tomas Babej <tomasbabej@gmail.com>
2015-02-15 16:50:55 +0100

    tests: Add new test for ActiveWindowPidReporter, use py.test style asserts

--- test/test_reporters.py
+++ test/test_reporters.py
@@ -7,12 +7,24 @@ class TimeReporterTest(ReporterTestCase):
     module_name = 'time'
 
     def test_time_reporter(self):
-        self.assertEqual(self.plugin.report(), datetime.now().strftime("%H.%M"))
+        time = self.plugin.report()
+        assert time == datetime.now().strftime("%H.%M")
 
 
-class TimeActiveWindowNameReporter(ReporterTestCase):
+class ActiveWindowNameReporterTest(ReporterTestCase):
     class_name = 'ActiveWindowNameReporter'
     module_name = 'active_window_name'
 
     def test_active_window_name_reporter(self):
-        self.plugin.report()
+        window = self.plugin.report()
+        assert type(window) == str
+        assert len(window) > 0
+
+
+class ActiveWindowPidReporterTest(ReporterTestCase):
+    class_name = 'ActiveWindowPidReporter'
+    module_name = 'active_window_pid'
+
+    def test_active_window_pid_reporter(self):
+        pid = self.plugin.report()
+        assert type(pid) == int
