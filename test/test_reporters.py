ccced3af30331fdf22ca14edb549e571be039b8f
Tomas Babej <tomasbabej@gmail.com>
2015-02-15 16:34:46 +0100

    tests: Move all Reporter tests into common file

--- test/test_reporters.py
+++ test/test_reporters.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 from test.base import ReporterTestCase
 
+
 class TimeReporterTest(ReporterTestCase):
     class_name = 'TimeReporter'
     module_name = 'time'
 
     def test_time_reporter(self):
         self.assertEqual(self.plugin.report(), datetime.now().strftime("%H.%M"))
+
+
+class TimeActiveWindowNameReporter(ReporterTestCase):
+    class_name = 'ActiveWindowNameReporter'
+    module_name = 'active_window_name'
+
+    def test_active_window_name_reporter(self):
+        self.plugin.report()
