b9e87c15e30f17fb08deaa957f954bf269576a8d
Tomas Babej <tomasbabej@gmail.com>
2015-02-15 19:03:31 +0100

    util: Add utils module

---
+++ util.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def run(args):
+    child = subprocess.Popen(args, stdout=subprocess.PIPE)
+    stdout, stderr = child.communicate()
+    rc = child.returncode
+
+    return stdout, stderr, rc
