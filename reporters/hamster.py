5ff7b6ed6f0b71a06da2ce4e088592e270e77f4e
Tomas Babej <tomasbabej@gmail.com>
2015-02-18 07:37:42 +0100

    reporters: Consolidate hamster reporters into one plugin

--- reporters/hamster.py
+++ reporters/hamster.py
@@ -8,7 +8,7 @@ class HamsterActivityReporter(IReporter):
     export_as = 'hamster'
 
     def __init__(self, **options):
-        super(IReporter, self).__init__(**options)
+        super(HamsterActivityReporter, self).__init__(**options)
         self.bus = dbus.SessionBus()
         self.bus.add_signal_receiver(
             self.get_current_activity,
@@ -34,3 +34,34 @@ class HamsterActivityReporter(IReporter):
 
     def report(self):
         return str(self.activity or '')
+
+
+class HamsterActivityDailyDurationReporter(IReporter):
+    export_as = 'hamster_activity_daily_duration'
+
+    def __init__(self, **options):
+        super(HamsterActivityDailyDurationReporter, self).__init__(**options)
+        self.bus = dbus.SessionBus()
+        proxy = dbus.SessionBus().get_object(
+            "org.gnome.Hamster", "/org/gnome/Hamster")
+        self.hamster = dbus.Interface(proxy, dbus_interface='org.gnome.Hamster')
+
+
+    def get_activity_duration(self):
+        totals = {}
+
+        for fact in self.hamster.GetTodaysFacts():
+            # 4 - name
+            # 6 - category
+            # 9 - duration
+            key = "%s@%s" % (fact[4], fact[6])
+
+            if key in totals:
+                totals[key] =+ fact[9]
+            else:
+                totals[key] = fact[9]
+
+        return totals
+
+    def report(self):
+        return self.get_activity_duration()
