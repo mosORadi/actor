4ca19d32cfde82737cff819fad2dcc83bcb5ca9c
Tomas Babej <tomasbabej@gmail.com>
2015-02-19 05:19:02 +0100

    fixers: Add SuspendUntilFixer

--- fixers/suspend.py
+++ fixers/suspend.py
@@ -31,6 +31,30 @@ class SuspendFixer(IFixer):
 
     def fix(self, **reports):
         if self.options.get('enforced'):
-            self.susupend_forced()
+            self.suspend_forced()
         else:
             self.suspend()
+
+
+import datetime
+
+
+class SuspendUntilFixer(IFixer):
+    """
+    Simple fixer that suspends your workstation,
+    until specified time, in %H.%M format.
+    """
+
+    export_as = "suspend_until"
+    required_plugin_options = ["until"]
+
+    def fix(self, **reports):
+        until_old = datetime.datetime.strptime(str(self.options['until']), "%H.%M")
+        until = datetime.datetime.combine(datetime.date.today(), until_old.time())
+
+        if datetime.datetime.now() > until:
+            until = until + datetime.timedelta(1)
+
+        seconds_until = int((until - datetime.datetime.now()).total_seconds())
+        print seconds_until
+        run(['sudo', 'rtcwake', '-u', '-m', 'mem', '-s', seconds_until])
