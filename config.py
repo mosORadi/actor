772ce33217825cf848caaf5f870a65051924cd3f
Tomas Babej <tomasbabej@gmail.com>
2015-03-03 23:48:12 +0100

    config: Change the location of configuration directory to ~/.actor

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 path_parts = os.path.dirname(os.path.realpath(__file__)).split('/')
 PROJECT_ROOT = '/'.join(path_parts) + '/'
 
-CONFIG_DIR = PROJECT_ROOT + 'config'
 HOME_DIR = os.path.expanduser('~')
+CONFIG_DIR = os.path.join(HOME_DIR, '.actor')
