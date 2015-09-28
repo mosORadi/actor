955ac6954a0f3ed6ae08fd13b07410b04d74da75
Tomas Babej <tomasbabej@gmail.com>
2015-09-28 23:42:10 +0200

    actor: Introduce Context and PluginDict

--- actor.py
+++ actor.py
@@ -19,6 +19,51 @@ from config import CONFIG_DIR, HOME_DIR
 from local_config import SLEEP_HASH
 
 
+class PluginDict(object):
+
+    def __init__(self, pluginmount, context, evaluate=False):
+        self.name = pluginmount.__name__
+        self.context = context
+        self.evaluate = evaluate
+        self.values = {}
+
+    @property
+    def plugins(self):
+        return {
+            plugin_class.__name__: plugin_class
+            for plugin_class in pluginmount.plugins
+        }
+
+    def __getitem__(self, name):
+        if name not in self.plugins:
+            raise ValueError("Could not find %s plugin of name: %s"
+                              % (self.name, name))
+
+        if self.evaluate:
+            if name not in self.values:
+                self.values[name] = self.plugins[name].evaluate()
+
+            return self.values[name]
+        else:
+            return self.plugins[name]
+
+
+    def clear(self):
+        self.values.clear()
+
+
+class Context(object):
+
+    def __init__(self):
+        self.rules = []
+        self.activity = None
+        self.flow = None
+
+        self.reporters = PluginDict(Reporter, self, evaluate=True)
+        self.checkers = PluginDict(Checker, self)
+        self.fixers = PluginDict(Fixer, self)
+
+
 class Actor(object):
 
     def __init__(self):
@@ -30,6 +75,7 @@ class Actor(object):
 
         # Load the plugins
         self.import_plugins()
+        self.context = Context()
 
         # Load the Actor configuration
         self.load_configuration()
