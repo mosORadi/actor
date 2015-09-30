46f3dd4a2eaa2e30f9e4931af8c6bbbc38bbe9e8
Tomas Babej <tomasbabej@gmail.com>
2015-09-30 23:49:05 +0200

    reporters: Refactor time reporters

--- reporters/time.py
+++ reporters/time.py
@@ -3,19 +3,24 @@ from plugins import Reporter
 
 
 class TimeReporter(Reporter):
+    """
+    Returns the current time, as the datetime object.
+    """
 
     identifier = 'time'
 
-    def report(self):
-        time = datetime.datetime.now()
-        return time.strftime("%H.%M")
+    def run(self):
+        return datetime.datetime.now()
 
 
 class WeekdayReporter(Reporter):
+    """
+    Returns the current week day, represented as string.
+    """
 
     identifier = 'weekday'
 
-    def report(self):
+    def run(self):
         day = datetime.datetime.now().strftime("%w")
 
         weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
