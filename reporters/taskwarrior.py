5fae9c2bb6b08be1b1b1290b6a4c14a5f5cd3944
Tomas Babej <tomasbabej@gmail.com>
2015-09-30 23:42:26 +0200

    reporters: Refactor Taskwarrior related reporters

--- reporters/taskwarrior.py
+++ reporters/taskwarrior.py
@@ -3,38 +3,18 @@ from tasklib import TaskWarrior
 
 
 class TaskWarriorReporter(Reporter):
+    """
+    Returns TaskWarrior tasks matching the given filter.
+    """
 
     identifier = 'tasks'
-    optional_plugin_options = ['filter', 'rawfilter', 'warrior_options']
 
-    def __init__(self, **options):
-        super(TaskWarriorReporter, self).__init__(**options)
-        self.task_filter = self.options.get('filter', dict())
-        self.raw_filter = self.options.get('rawfilter', [])
-        warrior_options = self.options.get('warrior_options', dict())
-        self.warrior = TaskWarrior(**warrior_options)
+    def report(self, warrior_options=None, rawfilter=None, taskfilter=None):
+        warrior_options = warrior_options or dict()
+        taskfilter = taskfilter or dict()
+        rawfilter = rawfilter or tuple()
 
-    def get_tasks(self):
-        return self.warrior.tasks.filter(*self.raw_filter, **self.task_filter)
+        warrior = TaskWarrior(**warrior_options)
+        tasks = warrior.filter(*rawfilter, **taskfilter)
 
-    def report(self):
-        return ';'.join([str(t['description']) for t in self.get_tasks()])
-
-
-class TasksCompletedReporter(TaskWarriorReporter):
-
-    identifier = 'tasks_completed'
-
-    def get_tasks(self):
-        return self.warrior.tasks.completed().filter(
-                *self.raw_filter,
-                **self.task_filter
-        )
-
-
-class TaskCountReporter(TaskWarriorReporter):
-
-    identifier = 'task_count'
-
-    def report(self):
-        return len(self.get_tasks())
+        return tasks
