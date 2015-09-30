27208ef9635a084b4215dbdab3757daea518b68c
Tomas Babej <tomasbabej@gmail.com>
2015-09-30 21:04:36 +0200

    reporters: Refactor FileContentReporter

--- reporters/file_content.py
+++ reporters/file_content.py
@@ -4,17 +4,15 @@ from plugins import Reporter
 
 
 class FileContentReporter(Reporter):
+    """
+    Returns the content of a given file.
+
+    Returns None if the file does not exist.
+    """
 
     identifier = 'file_content'
-    required_plugin_options = ['path']
 
-    def __init__(self, **options):
-        super(FileContentReporter, self).__init__(**options)
-        self.path = self.options.get('path', '')
-
-    def report(self):
-        if os.path.isfile(self.path):
-            with open(self.path, 'r') as f:
+    def run(self, path):
+        if os.path.isfile(path):
+            with open(path, 'r') as f:
                 return f.read()
-        else:
-            return ''
