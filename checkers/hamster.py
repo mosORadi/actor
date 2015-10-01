a039ef4ed9b97a4c1753813e1b45180c09268eaf
Tomas Babej <tomasbabej@gmail.com>
2015-10-01 09:01:08 +0200

    checkers: Refactor hamster checkers

--- checkers/hamster.py
+++ checkers/hamster.py
@@ -5,32 +5,18 @@ class HamsterActivityDailyDurationChecker(Checker):
     Checks whether a time limit for a particular activity has not been
     exceeded.
 
-    Expects reports:
-      - hamster_activity_daily_duration - Input dictionary of summary of
-                                          activity duration
-
-    Options:
-      - limit : The activity duration limit, in minutes
+    Following keywords are expected:
       - activity: The activity that should be measured, in the form of activity@Category
+      - limit : The activity duration limit, in minutes
     """
 
     identifier = 'hamster_activity_duration'
-    required_plugin_options = ['limit', 'activity']
 
-    def __init__(self, **options):
-        super(HamsterActivityDailyDurationChecker, self).__init__(**options)
-        self.activity = self.options.get('activity')
-        self.limit = self.options.get('limit', 0) * 60
+    def run(self, activity, limit):
+        duration = self.context.report('hamster_activity_daily_duration',
+                                       activity=activity)
 
-        if not self.activity:
-            raise ValueError("You must specify the activity option for HamsterActivityDailyDurationChecker")
-        if not self.limit:
-            raise ValueError("You must specify the limit option for HamsterActivityDailyDurationChecker")
-
-    def check(self, **reports):
-        duration = reports['hamster_activity_daily_duration'].get(self.activity, 0)
-
-        return duration > self.limit
+        return duration > limit
 
 
 class HamsterCategoryDailyDurationChecker(Checker):
@@ -38,34 +24,20 @@ class HamsterCategoryDailyDurationChecker(Checker):
     Checks whether a time limit for a particular activity has not been
     exceeded.
 
-    Expects reports:
-      - hamster_activity_daily_duration - Input dictionary of summary of
-                                          activity duration
-
-    Options:
-      - limit : The activity duration limit, in minutes
-      - category: The activity that should be measured, in the form of 'Category'
+    Following keywords are necessary:
+    - category: The category to be summed up
+    - limit: Maximum limit, in minutes
     """
 
     identifier = 'hamster_category_duration'
-    required_plugin_options = ['limit', 'category']
 
-    def __init__(self, **options):
-        super(HamsterCategoryDailyDurationChecker, self).__init__(**options)
-        self.category = self.options.get('category')
-        self.limit = self.options.get('limit', 0) * 60
-
-        if not self.category:
-            raise ValueError("You must specify the activity option for HamsterCategoryDailyDurationChecker")
-        if not self.limit:
-            raise ValueError("You must specify the limit option for HamsterCategoryDailyDurationChecker")
-
-    def check(self, **reports):
+    def run(self, category, limit):
         category_duration = 0
-        activities = reports['hamster_activity_daily_duration'].iteritems()
 
-        for activity, duration in activities:
-            if activity.endswith(self.category):
+        activities = self.context.report('hamster_activity_daily_duration')
+
+        for activity, duration in activities.iteritems():
+            if activity.endswith(category):
                 category_duration =+ duration
 
-        return category_duration > self.limit
+        return category_duration > limit
