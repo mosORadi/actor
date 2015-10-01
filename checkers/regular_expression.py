6205005a85801675c825d1f873a35f3e861f02e1
Tomas Babej <tomasbabej@gmail.com>
2015-10-01 09:37:12 +0200

    checkers: Refactor regular expression checker

--- checkers/regular_expression.py
+++ checkers/regular_expression.py
@@ -1,25 +1,19 @@
+import re
+
 from plugins import Checker
 
-import re
 
 class RegularExpressionChecker(Checker):
     """
-    Evaluates whether the input contains at least one
-    occurence of the pattern searched for.
+    Evaluates whether the input contains at least one occurence of the
+    pattern searched for.
 
-    Expects reports:
-      - string - Input string
-
-    Options:
-      - regexp : The string containing the regular expression
+    Expects following keyword arguments:
+      - regexp - Regular expression
+      - string - Input string tested to match the regular expression
     """
 
     identifier = 'regular_expression'
-    required_plugin_options = ['regexp']
 
-    def __init__(self, **options):
-        super(RegularExpressionChecker, self).__init__(**options)
-        self.re = re.compile(self.options.get('regexp'))
-
-    def check(self, **reports):
-        return bool(re.search(self.re, reports['string'] or ''))
+    def run(self, regexp, string):
+        return bool(re.search(regexp, string or ''))
