7d3d9e770ea66efcf38cf5843839ecbe2b4e5424
Tomas Babej <tomasbabej@gmail.com>
2015-10-01 09:21:33 +0200

    checkers: Refactor health checkers

--- checkers/health.py
+++ checkers/health.py
@@ -1,47 +1,49 @@
 from plugins import Checker
-import dbus
 import datetime
 
 
 class HealthChecker(Checker):
     """
     Returns false unless the checker has no health left. Damage is taken by
-    receiving D-Bus HealthDecreseSignal on org.freedesktop.AcTor interface.
+    calling 'decrease' method. It can be also increased by using the
+    'increase' method.
 
-    The health amount is reset on each day.
+    The health amount is reset on each day, unless reset_daily flag is set
+    to False during initlialization.
 
-    Expected options:
-        delay - coundown duration in seconds
-        id - identifier of countdown checker, should be unique across all
-             activities
+    Expected setup options:
+        maximum - the maximum amount of HP
     """
 
     identifier = 'health'
-    required_plugin_options = ['health_amount', 'id']
+    stateless = False
 
-    def __init__(self, **options):
+    def __init__(self, maximum, reset_daily=True):
         super(HealthChecker, self).__init__(**options)
-
-        self.bus = dbus.SessionBus()
-        self.bus.add_signal_receiver(
-            self.decrease_health,
-            dbus_interface="org.freedesktop.AcTor",
-            signal_name="HealthDecreaseSignal")
-
-        self.health_id = self.options.get('id')
-        self.health_remaining = self.options.get('health_amount', 0)
         self.today = datetime.date.today()
+        self.maximum = maximum
+        self.reset_daily = reset_daily
 
-    def decrease_health(self, health_id, **options):
-        if self.health_id != health_id:
+    def decrease(self, delta=1):
+        self.health = self.health - delta
+
+    def increase(self, delta=1):
+        self.health = self.health + delta
+
+    def reset(self):
+        self.health = self.maximum
+
+    def check_day_reset(self):
+        # Do not do anything if we're not configured to reset daily
+        if not self.reset_daily:
             return
 
-        if datetime.date.today() != self.today:
+        if datetime.date.today() > self.today:
             self.today = datetime.date.today()
-            self.health_remaining = self.options.get('health')
+            self.reset()
 
-        self.health_remaining = self.health_remaining - 1
+    def run(self):
+        # Check the daily reset first
+        self.check_day_reset()
 
-    def check(self, **reports):
-        self.debug("Remaining! : %s" % str(self.health_remaining))
-        return self.health_remaining < 0
+        return self.health < 0
