e03b0012e62db54ac4660aafc3e1f40dde2880c6
Tomas Babej <tomasbabej@gmail.com>
2015-10-04 16:01:55 +0200

    PluginCache: Document methods

--- actor.py
+++ actor.py
@@ -25,6 +25,26 @@ class HashableDict(dict):
 
 
 class PluginCache(object):
+    """
+    Provides an interface to the plugins of a particular type (identified by
+    given PluginMount class).
+
+    Never initializes the same class twice, once initialized, class instance
+    stays in the cache.
+
+    PluginCache tries to be smart and it will not try to evaluate the same
+    plugin when called with the same arguments as it has been called before.
+    This behaviour is leveraged by Actor to avoid redundat multiple
+    re-evaluations of the same plugin. It can be altered, however, with the
+    following two flags:
+
+      * stateless: Plugin is marked as stateless, i.e. its run() method
+                   does not keep any state inside the plugin object.
+                   If plugin is stateless, its instance can be shared between
+                   multiple Rules.
+      * side_effects: Plugin has side effects, i.e. it makes sense to
+                      re-evaluate it when called with the same arguments.
+    """
 
     def __init__(self, mount, context):
         self.mount = mount
@@ -37,6 +57,14 @@ class PluginCache(object):
         }
 
     def get(self, identifier, args, kwargs, rule_name=None):
+        """
+        Obtain a result from the given plugin. If the plugin is stateless
+        and has no side effects, it will be returned from the result cache.
+
+        Otherwise, the plugin will be re-evaluated and the result will be
+        returned.
+        """
+
         plugin_class = self.get_plugin(identifier)
 
         # Instances can be shared, and be kept for the time the Actor runs,
@@ -60,6 +88,10 @@ class PluginCache(object):
                                             class_identifier=identifier)
 
     def get_plugin(self, identifier):
+        """
+        Returns a plugin class identified by the given identifier.
+        """
+
         try:
             return self.plugins[identifier]
         except KeyError:
@@ -67,6 +99,15 @@ class PluginCache(object):
             # TODO: Raise an error, no such plugin available
 
     def get_plugin_instance(self, identifier, class_identifier):
+        """
+        Returns a plugin instance corresponding to the given class identifier.
+        If case the class identifier is None, identifier will be used. This is
+        useful for the stateful plugins, where an unique identifier is
+        constructed for each Rule.
+
+        Makes sure plugins are initialized only once.
+        """
+
         instance = self.instances.get(identifier)
 
         if instance is None:
@@ -76,6 +117,12 @@ class PluginCache(object):
         return instance
 
     def result_from_cache(self, identifier, args, kwargs):
+        """
+        Only for stateless plugins with no side-effects. Gets the result from
+        evaluation of the plugin with the corresponding identifier.
+
+        Makes sure plugins are evaluated only once per (args, kwargs) tuple.
+        """
         # Note: Only for stateless and no side-effects
 
         key = (identifier, args, HashableDict(**kwargs))
@@ -88,11 +135,19 @@ class PluginCache(object):
         return value
 
     def run_plugin_instance(self, identifier, args, kwargs, class_identifier=None):
-        # Meant for stateful or stateless with sideeffects
+        """
+        Evaluates the plugin instance corresponding to the given identifier
+        and returns the result.
+        """
+
         instance = self.get_plugin_instance(identifier, class_identifier)
         return instance.run(*args, **kwargs)
 
     def clear(self):
+        """
+        Clears the result cache. Instance cache is preserved.
+        """
+
         self.cache.clear()
 
     def __iter__(self):
