47c9fcdeb375a3cfcdb805d863d7284d05b7f6c9
Tomas Babej <tomasbabej@gmail.com>
2016-01-03 00:27:59 +0100

    fixers: Add prompt fixer implementation

---
+++ fixers/prompt.py
@@ -0,0 +1,14 @@
+from plugins import Fixer, DBusMixin, AsyncEvalMixin
+
+
+class PromptFixer(AsyncEvalMixin, DBusMixin, Fixer):
+
+    stateless = False
+
+    identifier = 'prompt'
+
+    bus_name = 'org.freedesktop.ActorDesktop'
+    object_path = '/Desktop'
+
+    def run(self, ident, message):
+        return self.interface.Prompt(ident, message)
