5c866efae1da99a1db1a624e1069487916a3ca32
Tomas Babej <tomasbabej@gmail.com>
2016-01-10 19:42:42 +0100

    desktop: Add simple overlay window, accessible over DBus

--- desktop.py
+++ desktop.py
@@ -10,6 +10,93 @@ import PyQt5.QtGui
 import PyQt5.QtCore
 
 
+class OverlayWindow(PyQt5.QtWidgets.QMainWindow):
+
+    def __init__(self, app, *args, **kwargs):
+
+        super(OverlayWindow, self).__init__(*args, **kwargs)
+
+        self.app = app
+
+        self.setObjectName("OverlayWindow")
+        self.resize(1920, 1080)
+
+        self.create_central_widget()
+        self.create_central_frame()
+        self.create_main_label()
+        self.create_input()
+        self.create_button()
+
+        self.set_window_flags()
+
+    def create_central_widget(self):
+        self.centralwidget = PyQt5.QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.setCentralWidget(self.centralwidget)
+
+    def create_central_frame(self):
+        self.frame = PyQt5.QtWidgets.QFrame(self.centralwidget)
+        self.frame.setObjectName("frame")
+        self.frame.setGeometry(PyQt5.QtCore.QRect(0, 0, 400, 400))
+        self.frame.setFrameShape(PyQt5.QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(PyQt5.QtWidgets.QFrame.Raised)
+        self.frame.setStyleSheet("QFrame {"
+            "background-color: rgba(255, 255, 255, 15); "
+            "}"
+        )
+
+        # Center the frame
+        qr = self.frame.frameGeometry()
+        cp = PyQt5.QtWidgets.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.frame.move(qr.topLeft())
+
+        self.layout().addWidget(self.frame)
+
+    def create_input(self):
+        self.lineEdit = PyQt5.QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setGeometry(PyQt5.QtCore.QRect(150, 150, 100, 30))
+
+    def create_button(self):
+        self.pushButton = PyQt5.QtWidgets.QPushButton(self.frame)
+        self.pushButton.setText("Finished")
+        self.pushButton.setShortcut("Q")
+        self.pushButton.clicked.connect(self.app.quit)
+        self.pushButton.setGeometry(PyQt5.QtCore.QRect(150, 200, 100, 30))
+
+    def create_main_label(self):
+        # Create the button
+        self.label = PyQt5.QtWidgets.QLabel("Hello world", self.frame)
+        self.label.setGeometry(PyQt5.QtCore.QRect(0, 0, 400, 150))
+        self.label.setAlignment(PyQt5.QtCore.Qt.AlignCenter)
+
+        # This is a necessary override, since otherwise it seems
+        # the background color opaqueness coefficient is summed up
+        # with the underlying QFrame
+        self.label.setStyleSheet("QLabel {"
+            "background-color: rgba(255, 255, 255, 0); "
+            "color: gray; "
+            "font-size: 24pt; "
+            "font-weight: bold; "
+            "}"
+        )
+
+    def paintEvent(self, event=None):
+        painter = PyQt5.QtGui.QPainter(self)
+
+        painter.setOpacity(0.85)
+        painter.setBrush(PyQt5.QtCore.Qt.black)
+        painter.setPen(PyQt5.QtGui.QPen(PyQt5.QtCore.Qt.black))
+        painter.drawRect(self.rect())
+
+    def set_window_flags(self):
+        self.setWindowFlags(PyQt5.QtCore.Qt.FramelessWindowHint)
+        self.setWindowFlags(PyQt5.QtCore.Qt.WindowStaysOnTopHint)
+        self.setAttribute(PyQt5.QtCore.Qt.WA_NoSystemBackground, True)
+        self.setAttribute(PyQt5.QtCore.Qt.WA_TranslucentBackground, True)
+
+
 class AsyncPromptThreadBase(PyQt5.QtCore.QThread):
 
     def __init__(self, desktop, reply_handler, message, identifier):
@@ -57,6 +144,22 @@ class AsyncPromptYesNoThread(AsyncPromptThreadBase):
         self.reply_handler(bool(value))
 
 
+class AsyncOverlayThread(AsyncPromptThreadBase):
+
+    def __init__(self, desktop, *args, **kwargs):
+        super(AsyncOverlayThread, self).__init__(desktop, *args, **kwargs)
+        self.communicator.prompted.connect(desktop.overlay)
+
+    # pyqtSignals need to be class attributes of class inheriting from QObject
+    class Communicator(PyQt5.QtCore.QObject):
+        prompted = PyQt5.QtCore.pyqtSignal(str, str)
+        received = PyQt5.QtCore.pyqtSignal(str)
+
+    @PyQt5.QtCore.pyqtSlot(str)
+    def return_result(self, value):
+        self.reply_handler(str(value))
+
+
 class ActorDesktopDBusProxy(dbus.service.Object):
 
     def __init__(self, desktop):
@@ -85,8 +188,18 @@ class ActorDesktopDBusProxy(dbus.service.Object):
     def PromptYesNo(self, message, identifier, reply_handler, error_handler):
         self.prompt_generic(AsyncPromptYesNoThread, message, identifier, reply_handler)
 
+    @dbus.service.method("org.freedesktop.ActorDesktop", in_signature='ss', out_signature='s',
+                         async_callbacks=('reply_handler', 'error_handler'))
+    def Overlay(self, title, description, reply_handler, error_handler):
+        self.prompt_generic(AsyncOverlayThread, title, description, reply_handler)
+
 
 class ActorDesktop(PyQt5.QtWidgets.QWidget):
+
+    def __init__(self, app):
+        super(ActorDesktop, self).__init__()
+
+        self.app = app
 
     @PyQt5.QtCore.pyqtSlot(str, str)
     def prompt_input(self, message, identifier):
@@ -111,6 +224,13 @@ class ActorDesktop(PyQt5.QtWidgets.QWidget):
         else:
             self.sender().received.emit(False)
 
+    @PyQt5.QtCore.pyqtSlot(str, str)
+    def overlay(self, title, description):
+        self.window = OverlayWindow(self.app)
+        self.window.showFullScreen()
+        return "Success"
+
+
 def main():
     # Start dbus mainloop, must happen before definition of the main app
     dbus.mainloop.pyqt5.DBusQtMainLoop(set_as_default=True)
@@ -120,7 +240,7 @@ def main():
     # Prevent input dialog from causing termination of the whole app
     app.setQuitOnLastWindowClosed(False)
 
-    desktop = ActorDesktop()
+    desktop = ActorDesktop(app)
     proxy = ActorDesktopDBusProxy(desktop)
     sys.exit(app.exec_())
 
