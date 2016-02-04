b1e3dc3ad5dc105b37ca614f2554e1144a61ca5f
Tomas Babej <tomasbabej@gmail.com>
2016-02-04 23:04:54 +0100

    tests: Remove the old cruft in the PluginTestCase

--- test/base.py
+++ test/base.py
@@ -23,23 +23,17 @@ class PluginTestCase(TestCase):
     module_name = None
     plugin_type = None
 
-    def __init__(self, *args, **kwargs):
-        self.options = {}
-        super(PluginTestCase, self).__init__(*args, **kwargs)
-
     def setUp(self):
-        # Update settings from the parent class instance
-        parent_options = getattr(self.__class__, 'options', dict())
-        self.options.update(parent_options)
-
         # Load the plugin
         module_name = '{0}s.{1}'.format(self.plugin_type, self.module_name)
         module = importlib.import_module(module_name)
-        self.plugin_class = getattr(module, self.class_name)
-        self.initialize(**self.options)
+        plugin_class = getattr(module, self.class_name)
 
-    def initialize(self, **options):
-        self.plugin = self.plugin_class(None)
+        # Initialize a new MockContext
+        self.context = MockContext()
+
+        # Instantiate the plugin using the MockContext
+        self.plugin = plugin_class(self.context)
 
 
 class ReporterTestCase(PluginTestCase):
