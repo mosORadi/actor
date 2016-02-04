b960be0a45da32b9517b4794145ec39b334a4815
Tomas Babej <tomasbabej@gmail.com>
2016-02-04 23:25:06 +0100

    tests: Refactor the TestIntervalChecker test

--- test/test_checkers.py
+++ test/test_checkers.py
@@ -1,5 +1,7 @@
 from test.base import CheckerTestCase
 
+from util import convert_timestamp
+
 
 class RegularExpressionCheckerTest(CheckerTestCase):
     class_name = 'RegularExpressionChecker'
@@ -14,21 +16,41 @@ class RegularExpressionCheckerTest(CheckerTestCase):
 class TimeIntervalCheckerTest(CheckerTestCase):
     class_name = 'TimeIntervalChecker'
     module_name = 'time_interval'
-    options = {'start': "18.00", 'end': "19.00"}
 
     def test_time_interval_checker(self):
-        assert self.plugin.check(time='18.50') == True
-        assert self.plugin.check(time='18.00') == True
-        assert self.plugin.check(time='19.00') == False
-        assert self.plugin.check(time='17.00') == False
+        self.context.reporters['time'] = convert_timestamp('18.00')
+        assert self.plugin.run(start='18.00', end='19.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('18.50')
+        assert self.plugin.run(start='18.00', end='19.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('19.00')
+        assert self.plugin.run(start='18.00', end='19.00') == False
+
+        self.context.reporters['time'] = convert_timestamp('17.30')
+        assert self.plugin.run(start='18.00', end='19.00') == False
 
     def test_time_interval_checker_overnight(self):
-        self.initialize(start="18.00", end="05.00")
-        assert self.plugin.check(time='17.00') == False
-        assert self.plugin.check(time='18.00') == True
-        assert self.plugin.check(time='18.50') == True
-        assert self.plugin.check(time='19.00') == True
-        assert self.plugin.check(time='00.00') == True
-        assert self.plugin.check(time='04.59') == True
-        assert self.plugin.check(time='05.00') == False
-        assert self.plugin.check(time='09.00') == False
+        self.context.reporters['time'] = convert_timestamp('17.00')
+        assert self.plugin.run(start='18.00', end='05.00') == False
+
+        self.context.reporters['time'] = convert_timestamp('18.00')
+        assert self.plugin.run(start='18.00', end='05.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('18.50')
+        assert self.plugin.run(start='18.00', end='05.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('19.00')
+        assert self.plugin.run(start='18.00', end='05.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('00.00')
+        assert self.plugin.run(start='18.00', end='05.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('04.59')
+        assert self.plugin.run(start='18.00', end='05.00') == True
+
+        self.context.reporters['time'] = convert_timestamp('05.00')
+        assert self.plugin.run(start='18.00', end='05.00') == False
+
+        self.context.reporters['time'] = convert_timestamp('09.00')
+        assert self.plugin.run(start='18.00', end='05.00') == False
