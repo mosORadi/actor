1a45c97270cd19758c27292ce5f16824c48db193
Tomas Babej <tomasbabej@gmail.com>
2016-06-18 15:31:31 +0200

    reporters: Add timewarrior duration reporter

--- reporters/timewarrior.py
+++ reporters/timewarrior.py
@@ -17,3 +17,25 @@ class TaskWarriorReporter(Reporter):
             return parts[1].strip()
         else:
             return None
+
+
+class TaskWarriorReporterDuration(Reporter):
+    """
+    Returns TaskWarrior tasks matching the given filter.
+    """
+
+    identifier = 'timew_activity_duration'
+
+    def run(self):
+        stdout, stderr, code = run(['timew'])
+        parts = stdout.splitlines()[3].split("Total ")
+
+        if len(parts) > 1:
+            segments = parts[1].strip().split(':')
+            if len(segments) > 1:
+                minutes = int(segments[1].lstrip('0') or 0)
+                hours = int(segments[0].lstrip('0') or 0)
+
+                return (hours * 60) + minutes
+
+        return None
