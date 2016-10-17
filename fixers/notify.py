cb0140e0983cfce7eea40354b9b1ed65aa7b8841
Tomas Babej <tomasbabej@gmail.com>
2016-10-17 15:46:15 +0200

    fixers: Make NotifyFixer more robust to the absence of the interface

--- fixers/notify.py
+++ fixers/notify.py
@@ -1,4 +1,5 @@
-import datetime
+import dbus
+
 from plugins import Fixer, DBusMixin
 from util import Periodic
 
@@ -30,9 +31,17 @@ class NotifyFixer(DBusMixin, Fixer):
     def run(self, message, headline="Actor Alert!",
             app_name="Actor", app_icon='', timeout=0):
         # pylint: disable=arguments-differ
-        if self.timer:
+
+        def notify():
+            if not self.timer:
+                return
+
             replaces_id = self.last_notification
-            self.last_notification = self.interface.Notify(
-                app_name, replaces_id, app_icon,
-                headline, message, [], {}, timeout
-            )
+            self.last_notification = self.interface.Notify(app_name, replaces_id, app_icon,
+                                                           headline, message, [], {},
+                                                           timeout)
+        try:
+            notify()
+        except dbus.DBusException:
+            self.initialize_interface()
+            notify()
