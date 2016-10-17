473e6483de3868e97a8a84a89b2d74f9a07b930a
Tomas Babej <tomasbabej@gmail.com>
2016-10-17 15:46:48 +0200

    fixers: Add Timew related fixers

--- fixers/set_activity.py
+++ fixers/set_activity.py
@@ -1,3 +1,6 @@
+import shlex
+import subprocess
+
 from plugins import Fixer, DBusMixin
 
 
@@ -31,3 +34,39 @@ class SetHamsterActivityFixer(DBusMixin, Fixer):
             # Zero stands for now
             self.interface.StopTracking(0)
             self.interface.AddFact(activity, 0, 0, False)
+
+
+class SetTimewActivityFixer(Fixer):
+    """
+    Simple fixer, that sets current activity in Timewarrior.
+
+    Accepted options (defaults in parentheses):
+      - activity: Activity string as put to timewarrior
+    """
+
+    identifier = "timew_set"
+
+    def run(self, activity):
+        # pylint: disable=arguments-differ
+
+        # First, let's detect the current activity to not
+        # redefine the same activity over and over
+        current_activity = self.report('timew_activity')
+
+        # We use substring search here to support setting activity
+        # of the form activity@Project, description
+        # since we generate only activity@Project
+        if current_activity is not activity:
+            subprocess.call(['timew', 'start'] + shlex.split(activity))
+
+
+class StopTimewActivityFixer(Fixer):
+    """
+    Simple fixer, that stops current activity in Timewarrior.
+    """
+
+    identifier = "timew_stop"
+
+    def run(self):
+        # pylint: disable=arguments-differ
+        subprocess.call(['timew', 'stop'])
