040142719bc6c54013e90064746a911c1ca68be8
Tomas Babej <tomasbabej@gmail.com>
2016-10-31 22:32:22 +0100

    setup: Exclude tests from included packages

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license='AGPLv3',
     url='https://github.com/tbabej/actor',
     download_url='https://github.com/tbabej/actor/downloads',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requirements,
     classifiers=[
